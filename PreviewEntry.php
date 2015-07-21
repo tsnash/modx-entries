de0fcd17a492b8d62907f072b40dc8bb5722c958
Tamel <tsnash@users.noreply.github.com>
2015-07-20 21:32:18 -0400

    Create PreviewEntry.php

---
+++ PreviewEntry.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'EntriesSettings.php';
+require_once 'EntriesFunctions.php';
+
+
+$columnValues = array();
+
+foreach ($ENTRIES_columns as $column => $type) {
+	if(isset($_POST[$column])) $columnValues[$column] = $_POST[$column];
+}
+
+$output = $modx->parseChunk($ENTRIES_preview_chunk, $columnValues, '[+', '+]');
+
+$output .= '\n<form name="entry" action="[~' . $ENTRIES_implant_resource .'~]" method="post">\n';
+$output .= '<input type="hidden" name="' . $ENTRIES_implant_mode . '" value="' . $_POST[$ENTRIES_implant_mode] . '>\n';
+
+foreach ($columnValues as $column => $value) {
+	$output .= '<input type="hidden" name="' . $column . '" value="' . $value . '">\n';
+}
+
+$output .= '<input type="button" onclick="history.back();" value="Back">\n';
+$output .= '<input type="submit" value="Submit">\n</form>';
+
+return $output;
+?>

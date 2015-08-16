8bed9c977c6af98fcb6966fb9f781510db9c689b
Tamel <tsnash@users.noreply.github.com>
2015-08-16 11:54:40 -0400

    Encouraging formatting _POST prior to usage
    getting hard to make sure these things make sense... also reeks of redundancy

--- snippets/PreviewEntry.php
+++ snippets/PreviewEntry.php
@@ -5,23 +5,24 @@ require_once 'includes/EntriesFunctions.php';
 
 $table = (isset($table) && array_key_exists($table, $ENTRIES_tables)) ? $table : $ENTRIES_default_table; //table entry will be sent to
 
-$columnValues = array();
+$columnValues = formatColumns($_POST);
+$row = array();
 
 foreach ($ENTRIES_tables[$table] as $column => $type) {
-	if(isset($_POST[$column])) $columnValues[$column] = $_POST[$column];
+	if(isset($columnValues[$column])) $row[$column] = $columnValues[$column];
 }
 
 //put form together for database submission
 $form = '\n<form name="entry" action="[~' . $ENTRIES_implant_resource .'~]" method="post">\n';
 $form .= '<input type="hidden" name="' . $ENTRIES_implant_mode . '" value="' . $_POST[$ENTRIES_implant_mode] . '>\n';
-foreach ($columnValues as $column => $value) {
+foreach ($row as $column => $value) {
 	$form .= '<input type="hidden" name="' . $column . '" value="' . $value . '">\n';
 }
 $form .= '<input type="button" onclick="history.back();" value="Back">\n';
 $form .= '<input type="submit" value="Submit">\n</form>';
 
-parseRow($table, $columnValues);
-$preview = $modx->parseChunk(chooseChunk($ENTRIES_preview_chunk, $table, $columnValues), $columnValues, '[+', '+]');
+parseRow($table, $row);
+$preview = $modx->parseChunk(chooseChunk($ENTRIES_preview_chunk, $table, $row), $row, '[+', '+]');
 
 $output = $preview .'\n'. $form;
 

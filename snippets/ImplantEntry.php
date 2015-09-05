a3f33de558573f873533075c721deccc91be14fd
Tamel <tsnash@users.noreply.github.com>
2015-09-04 21:07:36 -0400

    Mandated whitelisting for implant mode
    change the way function accesses post mode

--- snippets/ImplantEntry.php
+++ snippets/ImplantEntry.php
@@ -6,86 +6,89 @@ require_once 'includes/EntriesFunctions.php';
 
 $table = (isset($table) && array_key_exists($table, $ENTRIES_tables)) ? $mydb->escape($table) : $ENTRIES_default_table; //table to send entry to
 
-$mysqli = new mysqli($ENTRIES_host, $ENTRIES_implant_user, $ENTRIES_implant_pass, $ENTRIES_database);
-
-if (mysqli_connect_error()) {
-    die('Connect Error (' . mysqli_connect_errno() . ') '
-            . mysqli_connect_error());
-}
-
-$prepare = array('query' => '', 'bindTypes' => '', 'bindValues' => array(), 'bindParams' => array());
-
-//code to be used for preparing query for new entry
-if ($_POST[$ENTRIES_post_mode] == $ENTRIES_implant_new) {
-
-	//initial query text
-	$queryLHS = 'INSERT INTO ' . $ENTRIES_prefix . $table . ' (';
-	$queryRHS = 'VALUES (';
-
-	//get column data from POST array
-	foreach ($ENTRIES_tables[$table] as $column => $type) {
-		if(isset($_POST[$column])) { 
-			$queryLHS .= $column . ', ';
-			$queryRHS .= '?, ';
-			$prepare['bindTypes'] .= $type;
-			$prepare['bindValues'][] = $_POST[$column];
-
-		}
-	}
-
-	$prepare['query'] = trimAfterLoop($queryLHS, 2, ') ') . trimAfterLoop($queryRHS, 2, ')');
-
-}
-
-//code to be used for preparing query for edting an entry
-elseif ($_POST[$ENTRIES_post_mode] == $ENTRIES_implant_edit) {
+if ($mode = retrieveMode()) {
+	$mysqli = new mysqli($ENTRIES_host, $ENTRIES_implant_user, $ENTRIES_implant_pass, $ENTRIES_database);
 	
-	//initial query text
-	$queryLHS = 'UPDATE ' . $ENTRIES_prefix . $table . ' SET ';
-	$queryRHS = 'WHERE ';
-
-	//get column data from POST array
-	foreach ($ENTRIES_tables[$table] as $column => $type) {
-		if(isset($_POST[$column])) {
-			if ($column == $ENTRIES_key_columns[$table]) {
-				$queryRHS .= $column . '=?';
-			}
-			else {
-				$queryLHS .= $column . '=?, ';
+	if (mysqli_connect_error()) {
+	    die('Connect Error (' . mysqli_connect_errno() . ') '
+	            . mysqli_connect_error());
+	}
+	
+	$prepare = array('query' => '', 'bindTypes' => '', 'bindValues' => array(), 'bindParams' => array());
+	
+	//code to be used for preparing query for new entry
+	if ($mode == $ENTRIES_implant_new) {
+	
+		//initial query text
+		$queryLHS = 'INSERT INTO ' . $ENTRIES_prefix . $table . ' (';
+		$queryRHS = 'VALUES (';
+	
+		//get column data from POST array
+		foreach ($ENTRIES_tables[$table] as $column => $type) {
+			if(isset($_POST[$column])) { 
+				$queryLHS .= $column . ', ';
+				$queryRHS .= '?, ';
 				$prepare['bindTypes'] .= $type;
 				$prepare['bindValues'][] = $_POST[$column];
+	
 			}
 		}
+	
+		$prepare['query'] = trimAfterLoop($queryLHS, 2, ') ') . trimAfterLoop($queryRHS, 2, ')');
+	
 	}
-
-	$prepare['query'] = trimAfterLoop($queryLHS, 2, ' ') . $queryRHS;
 	
-	$prepare['bindTypes'] .= $ENTRIES_tables[$table][$ENTRIES_key_columns[$table]];
-	$prepare['bindValues'][] = $_POST[$ENTRIES_key_columns[$table]];
+	//code to be used for preparing query for edting an entry
+	elseif ($mode == $ENTRIES_implant_edit) {
+		
+		//initial query text
+		$queryLHS = 'UPDATE ' . $ENTRIES_prefix . $table . ' SET ';
+		$queryRHS = 'WHERE ';
 	
+		//get column data from POST array
+		foreach ($ENTRIES_tables[$table] as $column => $type) {
+			if(isset($_POST[$column])) {
+				if ($column == $ENTRIES_key_columns[$table]) {
+					$queryRHS .= $column . '=?';
+				}
+				else {
+					$queryLHS .= $column . '=?, ';
+					$prepare['bindTypes'] .= $type;
+					$prepare['bindValues'][] = $_POST[$column];
+				}
+			}
+		}
+	
+		$prepare['query'] = trimAfterLoop($queryLHS, 2, ' ') . $queryRHS;
+		
+		$prepare['bindTypes'] .= $ENTRIES_tables[$table][$ENTRIES_key_columns[$table]];
+		$prepare['bindValues'][] = $_POST[$ENTRIES_key_columns[$table]];
+		
+	}
+	
+	//prepare query
+	if($stmt = $mysqli->prepare($prepare['query'])) {
+		
+		//elaborate binding scheme
+		$prepare['bindParams'][] = &$prepare['bindTypes'];
+		foreach($prepare['bindValues'] as &$value) { 
+		    $prepare['bindParams'] = $value;
+		}
+		unset($value); //just in case
+		call_user_func_array(array($stmt, 'bind_param'), $prepare['bindParams']);
+	
+		if(!$stmt->execute()) printf("Error: %s.\n", $stmt->error);
+		else echo "Database updated successfully!<br>";
+	
+		$stmt->close();
+	
+	}
+	
+	else throw new ErrorException($mysqli->error, $mysqli->errno);
+	
+	$mysqli->close();
+	
+	return $modx->getChunk($ENTRIES_implant_success);
 }
 
-//prepare query
-if($stmt = $mysqli->prepare($prepare['query'])) {
-	
-	//elaborate binding scheme
-	$prepare['bindParams'][] = &$prepare['bindTypes'];
-	foreach($prepare['bindValues'] as &$value) { 
-	    $prepare['bindParams'] = $value;
-	}
-	unset($value); //just in case
-	call_user_func_array(array($stmt, 'bind_param'), $prepare['bindParams']);
-
-	if(!$stmt->execute()) printf("Error: %s.\n", $stmt->error);
-	else echo "Database updated successfully!<br>";
-
-	$stmt->close();
-
-}
-
-else throw new ErrorException($mysqli->error, $mysqli->errno);
-
-$mysqli->close();
-
-return $modx->getChunk($ENTRIES_implant_success);
 ?>
